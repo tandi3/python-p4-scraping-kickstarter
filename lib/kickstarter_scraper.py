c376fef40d84caadc0953da320729cbccad51763
Tandie <ysoyucho@gmail.com>
2025-09-22 14:41:54 +0300

    Complete Kickstarter scraper implementation

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
-import ipdb
 
 # projects: kickstarter.select("li.project.grid_4")[0]
 # title: project.select("h2.bbcard_name strong a")[0].text
@@ -10,7 +9,7 @@ import ipdb
 
 def create_project_dict():
     html = ''
-    with open('./fixtures/kickstarter.html') as file:
+    with open('../fixtures/kickstarter.html') as file:
         html = file.read()
     kickstarter = BeautifulSoup(html, 'html.parser')
     projects = {}
